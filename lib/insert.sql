ee1c8a590a27c8a56b1e4b75e354c8c56467b21a
Bret Hogg <hoggb@wwu.edu>
2017-10-23 18:45:10 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,8 +1,8 @@
-INSERT INTO bears (name) VALUES ("Mr. Chocolate", 2, "M", "brown", "angry", 1);
-INSERT INTO bears (name) VALUES ("Rowdy", 2, "M", "brown", "angry", 1);
-INSERT INTO bears (name) VALUES ("Tabitha", 2, "M", "brown", "angry", 1);
-INSERT INTO bears (name) VALUES ("Sergeant Brown", 2, "M", "brown", "angry", 1);
-INSERT INTO bears (name) VALUES ("Melissa", 2, "M", "brown", "angry", 1);
-INSERT INTO bears (name) VALUES ("Grinch", 2, "M", "brown", "angry", 1);
-INSERT INTO bears (name) VALUES ("Wendy", 2, "M", "brown", "angry", 1);
-INSERT INTO bears (name) VALUES (NULL, 2, "M", "brown", "angry", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 2, "M", "brown", "angry", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Rowdy", 2, "M", "brown", "angry", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Tabitha", 2, "M", "brown", "angry", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown", 2, "M", "brown", "angry", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Melissa", 2, "M", "brown", "angry", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Grinch", 2, "M", "brown", "angry", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Wendy", 2, "M", "brown", "angry", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL, 2, "M", "brown", "angry", 1);
