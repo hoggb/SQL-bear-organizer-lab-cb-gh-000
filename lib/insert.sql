e562d06bacc2e3d920f33edaff95ec0da95a4d32
Bret Hogg <hoggb@wwu.edu>
2017-10-23 18:29:11 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,1 +1,8 @@
 INSERT INTO bears (name) VALUES ("Mr. Chocolate", 2, "M", "brown", "angry", 1);
+INSERT INTO bears (name) VALUES ("Rowdy", 2, "M", "brown", "angry", 1);
+INSERT INTO bears (name) VALUES ("Tabitha", 2, "M", "brown", "angry", 1);
+INSERT INTO bears (name) VALUES ("Sergeant Brown", 2, "M", "brown", "angry", 1);
+INSERT INTO bears (name) VALUES ("Melissa", 2, "M", "brown", "angry", 1);
+INSERT INTO bears (name) VALUES ("Grinch", 2, "M", "brown", "angry", 1);
+INSERT INTO bears (name) VALUES ("Wendy", 2, "M", "brown", "angry", 1);
+INSERT INTO bears (name) VALUES (NULL, 2, "M", "brown", "angry", 1);
